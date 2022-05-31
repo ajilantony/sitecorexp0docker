4ea5b8ab3bb80e511788bea48cfde18b3a53876c
Ajil Antony <ajilantony@outlook.com>
2022-05-31 19:20:13 +0530

    first commit

--- enablevirtualmachine.ps1
+++
@@ -1,5 +0,0 @@
-#Enable the Windows Subsystem for Linux
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-
-#Enable Virtual Machine feature
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
